579690f85d361f2d669f2d37daa1e74546182b16
Victor Azevedo <josevcazevedo@gmail.com>
2022-08-26 12:26:13 -0300

    implemento get mensagens e atualizacao da pagina

--- scripts/script.js
+++ scripts/script.js
@@ -5,10 +5,13 @@ const urlParticipants =
   "https://mock-api.driven.com.br/api/v6/uol/participants";
 
 const urlStatus = "https://mock-api.driven.com.br/api/v6/uol/status";
+const urlMessages = "https://mock-api.driven.com.br/api/v6/uol/messages";
 
 const userName = { name: "" };
-let isLoggedIn = false;
+let messages;
+
 let intervalLoginID;
+let intervalMessagesGET;
 
 // *****************************************
 // Functions for Load Screens
@@ -30,6 +33,41 @@ function loadParticipantsBar() {
   document.querySelector(".container-participants").classList.remove("hidden");
 }
 
+function refreshChat() {
+  const chat = document.querySelector("main");
+  chat.innerHTML = "";
+
+  const typeOfMessage = {
+    status: "message-status",
+    message: "",
+    private_message: "message-private",
+  };
+
+  for (let i = 0; i < messages.length; i++) {
+    const messageFrom = messages[i].from;
+    let messageTo = messages[i].to;
+    const messageText = messages[i].text;
+    const messageType = messages[i].type;
+    const messageTime = messages[i].time;
+    let hiddenText = "";
+
+    if (messageType === "status") {
+      hiddenText = "hidden";
+    } else if (messageType === "private_message") {
+      messageTo = "reservadamente para " + messageTo;
+    } else {
+      messageTo = "para " + messageTo;
+    }
+    chat.innerHTML += `
+      <div class="box-message ${typeOfMessage[messageType]}">
+      <span class="message-time">(${messageTime}) </span>
+      <span class="message-from">${messageFrom} </span>
+      <sapn class="message-to ${hiddenText}">${messageTo}: </sapn>
+      <sapn class="message-text">${messageText} </sapn>
+      </div>;
+      `;
+  }
+}
 // *****************************************
 // Functions for Actions from User
 // *****************************************
@@ -45,22 +83,31 @@ function login(btn) {
 // *****************************************
 // Functions to API
 // *****************************************
-function sendUserOn() {
+function sendUserOnPOST() {
   const promessUserOn = axios.post(urlStatus, userName);
   promessUserOn.catch(errorUserOn);
 }
 
+// Load Messagens
+function loadMessagesGET() {
+  const promessLoadMessages = axios.get(urlMessages);
+  promessLoadMessages.then(loadMessages);
+}
+
 //*****************************************
 // Functions for API response
-// *****************************************
+// ****************************************
+// Login OK
 function processLogin(response) {
   if (response.status === 200) {
     loadChatScreen();
-    isLoggedIn = true;
-    intervalLoginID = setInterval(sendUserOn, 5000);
+    intervalLoginID = setInterval(sendUserOnPOST, 5000);
+    loadMessagesGET();
+    intervalMessagesGET = setInterval(loadMessagesGET, 3000);
   }
 }
 
+// Login ERROR
 function errorLogin(response) {
   if (response.response.status == 400) {
     alert("Usuário já exitente\nDigite um nome de usuário diferente");
@@ -75,6 +122,13 @@ function errorUserOn(response) {
     // // Recarrega tela de login
     // loadLoginScreen();
     console.log(response);
+  }
+}
+
+function loadMessages(response) {
+  if (response.status === 200) {
+    messages = response.data;
+    refreshChat();
   }
 }
 
